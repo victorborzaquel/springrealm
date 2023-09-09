f219b2852a8adef0981e0a3923ddecc5da789111
Victor Borzaquel <victorborzaquel@outlook.com>
2023-09-09 10:22:12 -0300

    feat(start project) first commit
    Start project with spring boot initializr

---
+++ src/main/java/com/victorborzaquel/springrealm/SpringrealmApplication.java
@@ -0,0 +1,13 @@
+package com.victorborzaquel.springrealm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrealmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrealmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorborzaquel/springrealm/SpringrealmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorborzaquel.springrealm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrealmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
