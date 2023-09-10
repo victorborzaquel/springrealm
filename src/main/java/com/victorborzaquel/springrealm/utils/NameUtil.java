1250a1d780c147cb4760440786b8417bcd2257be
Victor Borzaquel <victorborzaquel@outlook.com>
2023-09-10 14:07:27 -0300

    chore(NameUtil) create a utility for formatting and get full names

---
+++ src/main/java/com/victorborzaquel/springrealm/utils/NameUtil.java
@@ -0,0 +1,22 @@
+package com.victorborzaquel.springrealm.utils;
+
+public class NameUtil {
+  public static String capitalize(String name) {
+    return name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+  }
+
+  public static String capitalizeAll(String name) {
+    String[] names = name.split(" ");
+    String result = "";
+    for (String n : names) {
+      result += capitalize(n) + " ";
+    }
+    return result.trim();
+  }
+
+  public static String getFullName(String firstName, String lastName) {
+    if (lastName == null || lastName.isEmpty()) return capitalizeAll(firstName);
+
+    return capitalizeAll(firstName) + " " + capitalizeAll(lastName);
+  }
+}
