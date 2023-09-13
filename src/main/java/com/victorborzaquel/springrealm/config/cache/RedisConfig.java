e620e39e93a454d9368134eba78edae78a8683b2
Victor Borzaquel <victorborzaquel@outlook.com>
2023-09-13 13:42:51 -0300

    chore(config) add redis and swagger config

---
+++ src/main/java/com/victorborzaquel/springrealm/config/cache/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.victorborzaquel.springrealm.config.cache;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate(
+      LettuceConnectionFactory lettuceConnectionFactory,
+      ObjectMapper objectMapper) {
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+    redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+    redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+    redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+
+    return redisTemplate;
+  }
+}
