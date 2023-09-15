5a0afbdf0f91714230881bfa10737561f7d16ce6
Victor Borzaquel <victorborzaquel@outlook.com>
2023-09-14 23:39:54 -0300

    fix(docs) add production and localhost servers

--- src/main/java/com/victorborzaquel/springrealm/config/docs/OpenApiConfig.java
+++ src/main/java/com/victorborzaquel/springrealm/config/docs/OpenApiConfig.java
@@ -1,21 +1,35 @@
 package com.victorborzaquel.springrealm.config.docs;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
   @Bean
   OpenAPI defaultOpenApiConfig() {
     Info info = new Info();
-    
+
     info.setTitle("Victor's Springrealm: A Jornada Java");
     info.setDescription("API de Jogo RPG desenvolvida com Spring Boot.");
     info.setVersion("0.0.1: Alpha");
 
-    return new OpenAPI().info(info);
+    Server localhostServer = new Server();
+    localhostServer.setUrl("http://localhost:9988");
+    localhostServer.setDescription("Localhost");
+
+    Server productionServer = new Server();
+    productionServer.setUrl("https://api.victorborzaquel.com");
+    productionServer.setDescription("Production");
+
+    return new OpenAPI()
+        .info(info)
+        .addServersItem(localhostServer)
+        .addServersItem(productionServer);
   }
 }
